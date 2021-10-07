a77f187272320496d9fdaf55e98b05976cb93c26
gaitong <gaitong.p@gmail.com>
2021-10-07 11:36:55 +0700

    init my app

---
+++ NetCore-Quartz/HelloJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore_Quartz
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - Greetings from I Love my job!");
+        }
+    }
+}
